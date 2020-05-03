62571698c9f461505de68ff56aae7be3cadd778e
Javed Alam <javed@taazaa.com>
2020-05-03 11:57:57 +0530

    START -1

--- testB.php
+++ testB.php
@@ -1,12 +1,4 @@
 <?php
 
 echo $some;
-echo "Attempt - 2";
-echo "Attempt - 3";
-echo "Attempt - 4 a";
-echo "Attempt - 5 B";
-
-echo "Reverting Test - A -1 ";
-echo "Reverting add text ";
-echo "I want to revert it Before push";
-echo "Hello 2";
+echo "START  - 1";
